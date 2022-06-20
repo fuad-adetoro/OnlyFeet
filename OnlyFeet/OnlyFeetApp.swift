68d795a0aad5114f2c4d8cc6fe6fa2a9a1f568e4
Fuad Adetoro <fuad@inhaus.ai>
2022-06-20 22:00:10 +0100

    Initial Commit

---
+++ OnlyFeet/OnlyFeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyFeetApp.swift
+//  OnlyFeet
+//
+//  Created by Fuad on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlyFeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
