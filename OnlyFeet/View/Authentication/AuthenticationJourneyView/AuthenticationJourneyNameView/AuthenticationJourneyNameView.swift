36a8e4ee94ea8a2b5f04dff59d0f567551d74593
Fuad Adetoro <fuad@inhaus.ai>
2022-06-27 02:08:20 +0100

    Authentication Journey View Model tests

---
+++ OnlyFeet/View/Authentication/AuthenticationJourneyView/AuthenticationJourneyNameView/AuthenticationJourneyNameView.swift
@@ -0,0 +1,61 @@
+//
+//  AuthenticationJourneyNameView.swift
+//  OnlyFeet
+//
+//  Created by Fuad on 27/06/2022.
+//
+
+import SwiftUI
+
+struct AuthenticationJourneyNameView: View {
+    @EnvironmentObject var viewModel: AuthenticationJourneyViewModel
+    @Binding var displayName: String
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                VStack {
+                    HStack {
+                        Spacer().frame(width: 50)
+                        
+                        Text("My name is")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer().frame(height: 40)
+                    
+                    HStack {
+                        Spacer().frame(width: 50)
+                        
+                        TextFieldWithColoredPlaceholder.init(text: $displayName, placeholderText: .constant("Your name..."), placeholderColor: .constant(Color("CustomLightGray").opacity(0.4)), canDismissKeyboard: .constant(false))
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer().frame(width: 50)
+                        Rectangle.init().frame(width: 195, height: 4)
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    
+                    Spacer().frame(height: 40)
+                    
+                    HStack {
+                        Spacer().frame(width: 20)
+                        
+                        AuthenticationJourneyContinueButtonView.init(isInactive: .constant($displayName.wrappedValue.isEmpty))
+                            .environmentObject(viewModel)
+                        
+                        Spacer().frame(width: 20)
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }
+    }
+}
---
+++ OnlyFeetTests/AuthenticationJourneyViewModelTests/AuthenticationJourneyViewModelTests.swift
@@ -0,0 +1,55 @@
+//
+//  AuthenticationJourneyViewModelTests.swift
+//  OnlyFeetTests
+//
+//  Created by Fuad on 27/06/2022.
+//
+
+import XCTest
+@testable import OnlyFeet
+import SwiftUI
+
+class AuthenticationJourneyViewModelTests: XCTestCase {
+    private var viewModel: AuthenticationJourneyViewModel!
+    
+    override func setUp() {
+        self.viewModel = AuthenticationJourneyViewModel.shared
+    }
+    
+    override func tearDown() {
+        self.viewModel = nil
+    }
+    
+    func testTransitionChange() {
+        viewModel.nextJourney()
+        
+        let edge = viewModel.determinePresentStyle()
+        
+        XCTAssertEqual(edge, Edge.trailing)
+        
+        viewModel.previousJourney()
+        
+        let previousEdge = viewModel.determinePresentStyle()
+        
+        XCTAssertEqual(previousEdge, Edge.leading)
+    }
+    
+    func testNextJourney() {
+        viewModel.authenticationJourney = .name
+        
+        viewModel.nextJourney()
+        
+        XCTAssertEqual(viewModel.authenticationJourney, AuthenticationJourneyPosition.birthday)
+        XCTAssertEqual(viewModel.previousAuthenticationJourney, AuthenticationJourneyPosition.name)
+    }
+    
+    func testPreviousJourney() {
+        viewModel.authenticationJourney = .profilePhoto
+        
+        viewModel.previousJourney()
+        
+        XCTAssertEqual(viewModel.authenticationJourney, AuthenticationJourneyPosition.gender)
+        XCTAssertEqual(viewModel.previousAuthenticationJourney, AuthenticationJourneyPosition.profilePhoto)
+    }
+
+}
